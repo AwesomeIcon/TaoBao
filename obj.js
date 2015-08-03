e5ed7bfb40f1383ab6b6f246d2ce24d8a0ead62e
Mr.Huang <uestc_ccse@outlook.com>
2015-08-03 16:08:25 +0800

    数据恢复，还好有备份

---
+++ obj.js
@@ -0,0 +1,260 @@
+function changeit(objs){
+	objs.style.background="#f22e00";
+	objs.style.color="#fff";
+	objs.style.fontWeight="bold";
+}
+
+function recoverit(objs){
+	objs.style.background="#fff";
+	objs.style.color="#6c6c6c";
+	objs.style.fontWeight="normal";
+}
+
+function qingchu(objs){
+	objs.value="";
+}
+
+
+
+var tianmao=document.getElementById("tianmao");
+var baobei=document.getElementById("baobei");
+var dianpu=document.getElementById("dianpu");
+var sousuolan=document.getElementById("sousuolan");
+var button=document.getElementById("button");
+var searchtext=document.getElementById("searchtext");
+
+function tianmao1(){
+	sousuolan.id="sousuolan_";
+	button.id="button_";
+	searchtext.value="面膜疯狂囤每片3元起";
+	tianmao.id="tianmao_";
+	baobei.id="baobei_";
+	dianpu.id="dianpu";
+}
+
+function dianpu1(){
+	dianpu.id="dianpu_";
+	searchtext.value="";
+	button.id="button";
+	sousuolan.id="sousuolan";
+	baobei.id="baobei_";
+	tianmao.id="tianmao";
+}
+
+function baobei1(){
+	baobei.id="baobei";
+	button.id="button";
+	sousuolan.id="sousuolan";
+	searchtext.value="一大波灭蚊神器向你袭来！";
+	tianmao.id="tianmao";
+	dianpu.id="dianpu";
+}
+
+function bianse(objs){
+	objs.style.backgroundColor="#f22d00";
+}
+
+function tuise(objs){
+	objs.style.backgroundColor="#ff4400";
+}
+
+	
+var hidbar1=document.getElementById("hidbar1");
+
+function playbar1(objs){
+	hidbar1.style.height="30px";
+}
+
+function hiddbar1(objs){
+	hidbar1.style.height="0px";
+}
+
+var hidbar2=document.getElementById("hidbar2");
+
+function playbar2(objs){
+	hidbar2.style.height="30px";
+}
+
+function hiddbar2(objs){
+	hidbar2.style.height="0px";
+}
+
+var hidbar3=document.getElementById("hidbar3");
+
+function playbar3(objs){
+	hidbar3.style.height="30px";
+}
+
+function hiddbar3(objs){
+	hidbar3.style.height="0px";
+}
+
+var hidbar4=document.getElementById("hidbar4");
+
+function playbar4(objs){
+	hidbar4.style.height="30px";
+}
+
+function hiddbar4(objs){
+	hidbar4.style.height="0px";
+}
+
+var hidbar5=document.getElementById("hidbar5");
+
+function playbar5(objs){
+	hidbar5.style.height="30px";
+}
+
+function hiddbar5(objs){
+	hidbar5.style.height="0px";
+}
+
+var hidbar6=document.getElementById("hidbar6");
+
+function playbar6(objs){
+	hidbar6.style.height="30px";
+}
+
+function hiddbar6(objs){
+	hidbar6.style.height="0px";
+}
+
+var hidbar7=document.getElementById("hidbar7");
+
+function playbar7(objs){
+	hidbar7.style.height="30px";
+}
+
+function hiddbar7(objs){
+	hidbar7.style.height="0px";
+}
+
+var hidbar8=document.getElementById("hidbar8");
+
+function playbar8(objs){
+	hidbar8.style.height="30px";
+}
+
+function hiddbar8(objs){
+	hidbar8.style.height="0px";
+}
+
+var hidbar9=document.getElementById("hidbar9");
+
+function playbar9(objs){
+	hidbar9.style.height="30px";
+}
+
+function hiddbar9(objs){
+	hidbar9.style.height="0px";
+}
+
+var testa=document.getElementById("testa");
+
+function test1(objs){
+	testa.style.display="block";
+}
+
+function test2(objs){
+	testa.style.display="none";
+}
+
+var testb=document.getElementById("testb");
+
+function test3(objs){
+	testb.style.display="block";
+}
+
+function test4(objs){
+	testb.style.display="none";
+}
+
+var testc=document.getElementById("testc");
+
+function test5(objs){
+	testc.style.display="block";
+}
+
+function test6(objs){
+	testc.style.display="none";
+}
+
+var testd=document.getElementById("testd");
+
+function test7(objs){
+	testd.style.display="block";
+}
+
+function test8(objs){
+	testd.style.display="none";
+}
+
+var teste=document.getElementById("teste");
+
+function test9(objs){
+	teste.style.display="block";
+}
+
+function test10(objs){
+	teste.style.display="none";
+}
+
+var testf=document.getElementById("testf");
+
+function test11(objs){
+	testf.style.display="block";
+}
+
+function test12(objs){
+	testf.style.display="none";
+}
+
+var testg=document.getElementById("testg");
+
+function test13(objs){
+	testg.style.display="block";
+}
+
+function test14(objs){
+	testg.style.display="none";
+}
+
+var testh=document.getElementById("testh");
+
+function test15(objs){
+	testh.style.display="block";
+}
+
+function test16(objs){
+	testh.style.display="none";
+}
+
+var testi=document.getElementById("testi");
+
+function test17(objs){
+	testi.style.display="block";
+}
+
+function test18(objs){
+	testi.style.display="none";
+}
+
+var shuaxin=document.getElementById("shuaxin");
+
+function newen(objs){
+	shuaxin.src="images/shuaxin2.png";
+}
+
+function unnew(objs){
+	shuaxin.src="images/shuaxin1.png";
+}
+
+var shuaxin1=document.getElementById("shuaxin1");
+
+function newen1(objs){
+	shuaxin1.src="images/shuaxin2.png";
+}
+
+function unnew1(objs){
+	shuaxin1.src="images/shuaxin1.png";
+}
